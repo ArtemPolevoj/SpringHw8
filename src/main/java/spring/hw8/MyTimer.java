3941a80b40366c81894cf6346940174054aee4d1
artem <artempolevoj@yandex.ru>
2024-04-04 21:48:00 +0300

    add MyTimer.java

---
+++ src/main/java/spring/hw8/MyTimer.java
@@ -0,0 +1,21 @@
+package spring.hw8;
+
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class MyTimer {
+    public void start(int maxTime){
+        System.out.println("Timer starter");
+        for (int i = 1; i <= maxTime ; i++) {
+            System.out.println(i);
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println("Timer stopped");
+    }
+}
